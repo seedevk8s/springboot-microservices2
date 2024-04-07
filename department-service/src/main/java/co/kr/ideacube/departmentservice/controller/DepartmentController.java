7956d8856e3ed78596becb8e059477da24c7f83c
seedevk8s <seedevk8s@gmail.com>
2024-04-07 07:09:04 +0000

    first commit

---
+++ department-service/src/main/java/co/kr/ideacube/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,33 @@
+package co.kr.ideacube.departmentservice.controller;
+
+import lombok.AllArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import co.kr.ideacube.departmentservice.dto.DepartmentDto;
+import co.kr.ideacube.departmentservice.service.DepartmentService;
+
+@RestController
+@RequestMapping("api/departments")
+@AllArgsConstructor
+public class DepartmentController {
+
+    private DepartmentService departmentService;
+
+    // Build save department REST API
+    @PostMapping
+    public ResponseEntity<DepartmentDto> saveDepartment(@RequestBody DepartmentDto departmentDto){
+        DepartmentDto savedDepartment = departmentService.saveDepartment(departmentDto);
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+
+    // Build get department rest api
+    @GetMapping("{department-code}")
+    public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("department-code") String departmentCode){
+        DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
+        return new ResponseEntity<>(departmentDto, HttpStatus.OK);
+    }
+}
